b894d02c414476b87243d72ec63f0e802f952f7a
subavini <subashinisuba.v@gmail.com>
2017-06-30 16:15:24 +0530

    Create Pp.java

---
+++ Pp.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+public class Pp
+{
+public static void main(String[] args)
+{
+String str="Hello";
+int i;
+for(i=0;i<5;i++)
+{
+System.out.println(str);
+}
+}
+}
