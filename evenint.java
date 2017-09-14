c8e4c2381f77b2af2be6006eca78ab3022a6db2a
subavini <subashinisuba.v@gmail.com>
2017-09-14 14:43:40 +0530

    Create evenint.java

---
+++ evenint.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class evenint
+{
+public static void main(String args[])
+{
+int i;
+for(i=0;i<=50;i++)
+{
+if(i%2!=0){
+System.out.println("the odd num are" +i);
+}
+}
+}
+}
